592b5540999b0584925a529b3cbedf7324f24af1
301 <301@223.194.169.108>
2015-03-20 12:48:01 +0900

    adsfa

--- 11Project/src/exam/first/HelloServlet.java
+++ 11Project/src/exam/first/HelloServlet.java
@@ -3,6 +3,8 @@ package exam.first;
 import java.io.IOException;
 
 
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,9 +31,18 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	
+		
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
-		out.print("hello");
+		
+		String name = request.getParameter("name");
+		String i = request.getParameter("ID");
+		
+		out.print("hello - GEt<br>");
+		out.print("이름"+ name + "<br>");
+		out.print(i);
+		
 	}
 	
 
@@ -40,6 +51,20 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		String i = request.getParameter("ID");
+		String p = request.getParameter("pwd");
+		
+		
+		
+		out.print("hello - GEt<br>");
+		out.print("이름"+ name + "<br>");
+		out.print(i);
+		out.print(p);
 	}
 
 }
